598ded039be0ff305c4fe1ed2b4f5aa984b50463
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-06-08 18:34:45 -0300

    Install main packages, add minimal configuration to get the app up and running.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
