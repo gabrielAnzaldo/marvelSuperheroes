bedf13be85fe5b28bac8501a72bde6727ff70ecd
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-06-09 17:54:41 -0300

    Add md5 package, and move to constants respective keys.

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,15 @@
+import md5 from 'js-md5';
+
 const getSuperHeroes = () => {
-  // const ts = Date.now();
-  // const hash = 'c84dbeb65c97e41e0384a3031d287a61';
-  // const apikey = '65061638632bc5ed70370ec1344a2e1c';
+  // TODO: move to a .env configuration file
+  const ts = Date.now();
+  const privateKey = '1808ec67118dd1d84cdd0169d6d05ae9ae6d2059';
+  const publicapikey = '65061638632bc5ed70370ec1344a2e1c';
+  const hash = md5(`${ts}${privateKey}${publicapikey}`);
+  const parameters = `?apikey=${publicapikey}&hash=${hash}&ts=${ts}`;
+
   return fetch(
-    'https://gateway.marvel.com:443/v1/public/characters?apikey=65061638632bc5ed70370ec1344a2e1c&hash=c84dbeb65c97e41e0384a3031d287a61&ts=1560086041721',
+    `https://gateway.marvel.com:443/v1/public/characters${parameters}`,
   ).then(response => {
     return response.json();
   });
