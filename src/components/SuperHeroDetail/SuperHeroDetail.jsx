3cb971aafa425c66a718305a6929eef7b1b30bc9
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2019-06-09 17:22:25 -0300

    Complete the detailed view of a hero

--- src/components/SuperHeroDetail/SuperHeroDetail.jsx
+++ src/components/SuperHeroDetail/SuperHeroDetail.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uuidv4 from 'uuid/v4';
 import styles from './styles.scss';
 
-const SuperHeroDetail = ({ selectedSuperhero }) => (
-  <div className={styles.superHeroDetail}>
-    {selectedSuperhero ? (
-      <div> {selectedSuperhero.name}</div>
-    ) : (
-      <div>select your favorite hero!</div>
-    )}
-  </div>
-);
+const SuperHeroDetail = ({ selectedSuperhero }) => {
+  return (
+    <div className={styles.superHeroDetail}>
+      {selectedSuperhero ? (
+        <div className={styles.superHeroDetailInfo}>
+          <img
+            className={styles.superHeroDetailInfoItem}
+            src={`${selectedSuperhero.thumbnail.path}/portrait_xlarge.jpg`}
+            alt="hero info"
+          />
+          <div className={styles.superHeroDetailInfoItem}>
+            <b>{selectedSuperhero.name}</b>
+            <p>{selectedSuperhero.description}</p>
+            <div>
+              <ul>
+                {selectedSuperhero.urls.map(({ url, type }) => (
+                  <li key={uuidv4()}>
+                    <a href={url}>{type}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>select your favorite hero!</div>
+      )}
+    </div>
+  );
+};
 
 export default SuperHeroDetail;
 
